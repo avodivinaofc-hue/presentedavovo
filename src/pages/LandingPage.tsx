b5dd421a4e375d2104e8eaaa93adc31a0499cff6
Rodney L <rrodnney@gmail.com>
2025-09-12 06:03:25 -0300

    NOVA IMAGEM DA VOVÓ DIVINA - Deploy Final
     Atualizações implementadas:
    - Nova imagem da Vovó Divina (vovo-divina-nova.jpeg.png)
    - Referência atualizada no código
    - Build concluído com sucesso
    - Deploy pronto para produção
    
     Resultado: Página com nova imagem da Vovó Divina
     Status: Pronto para visualização

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -334,7 +334,7 @@ const LandingPage = () => {
             <div className="flex justify-center">
               <div className="relative">
                 <img 
-                  src="/lovable-uploads/vovo-divina-nova.jpg" 
+                  src="/lovable-uploads/vovo-divina-nova.jpeg.png" 
                   alt="Vovó Divina - Guardiã do conhecimento milenar"
                   className="w-48 h-48 object-cover rounded-full border-4 border-yellow-400 shadow-2xl"
                 />
