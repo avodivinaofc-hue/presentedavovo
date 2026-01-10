f8694b311eb0ab93ab8abd4254a3dfd498e76c1d
Oraculo Divino <user@oraculo-divino.com>
2026-01-09 21:53:42 -0300

    chore: Clear repository for fresh start

--- src/components/crystal/index.ts
+++
@@ -1,6 +0,0 @@
-export { BreathingCircle } from './BreathingCircle';
-export { CrystalParticles } from './CrystalParticles';
-export { HoldToReveal } from './HoldToReveal';
-export { JournalInput } from './JournalInput';
-export { ProgressMandala } from './ProgressMandala';
-export { ReadingProgressBar } from './ReadingProgressBar';
--- src/pages/LandingPage.tsx
+++
@@ -1,8 +0,0 @@
-import LandingPagePT from "./LandingPagePT";
-
-const LandingPage = () => {
-  // Render only Portuguese landing page
-  return <LandingPagePT />;
-};
-
-export default LandingPage;
